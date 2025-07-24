8339bc47046d55f70426e042ea772926925446b3
jesusresendiz56 <jesusres56@gmail.com>
2025-07-24 01:05:57 -0600

    local host

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,16 +1,14 @@
 <?php
 // conexion 1 de base de datos (form 1 tabla registro)
-$host = 'trolley.proxy.rlwy.net';
-$port = 49388;
-$user = 'root';
-$pass = 'OSjIyZStEWbFPNPMCcKZEMuPIxNnyjNL';
-$dbname = 'railway';
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "vaguettos";
 
 // Create connection 1 (registro)
-$mysqli = new mysqli($host, $user, $pass, $dbname, $port);
+$conn = mysqli_connect(hostname:$servername, username:$username, password:$password,database:$dbname);
 
-if ($mysqli->connect_error) {
-    die('Error de conexión (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
-} else {
-    echo "Conexión exitosa";
+if (!$conn) {
+    die("fallo la conexion: <br>" . mysqli_connect_error());
 }
+   // echo "Conexión exitosa.";
