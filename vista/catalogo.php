8ad9ec15ba75784667f37e03443eadcadef371d2
AlasGR <Alas-gomez2@hotmail.com>
2025-07-26 21:23:56 -0600

    Eliminar conexión local y mantener solo la conexión remota en catalogo.php

--- vista/catalogo.php
+++ vista/catalogo.php
@@ -1,12 +1,8 @@
 <?php
 // --- Incluir conexiones ---
-<<<<<<< HEAD
-include '../modelo/conexion.php';   // local
-//include '../modelo/conexion2.php';  // remota
-=======
 //include '../modelo/conexion.php';   // local
 include '../modelo/conexion2.php';  // remota
->>>>>>> 6f2435c0b32dfc9757902a616ac07086123bae1e
+
 
 // --- Cambiar aquí a false para usar local ---
 $usarRemoto = false;
